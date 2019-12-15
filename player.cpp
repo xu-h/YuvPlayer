0cd96cd4cd8bd2ecd0a41646be33e42c4d2ce10a
xuh <hanxu727@gmail.com>
2019-12-15 15:09:35 +0800

    open a specific yuv file and read the Y component of the first frame.

--- player.cpp
+++ player.cpp
@@ -1,15 +1,54 @@
 #include "player.h"
 #include "ui_player.h"
 
+#include <QFile>
+#include <QDataStream>
+#include <QPixmap>
+
 Player::Player(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Player)
 {
     ui->setupUi(this);
+
+    width = 1920;
+    height = 1080;
+
+    yuv[0] = new QImage(width, height, QImage::Format_Grayscale8);
+    yuv[1] = new QImage(width >> 1, height >> 1, QImage::Format_Grayscale8);
+    yuv[2] = new QImage(width >> 1, height >> 1, QImage::Format_Grayscale8);
+
+    imageLabel = new QLabel();
+    imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+
+    ui->scrollArea->setWidget(imageLabel);
+    ui->scrollArea->setBackgroundRole(QPalette::Dark);
+
 }
 
 Player::~Player()
 {
     delete ui;
+
+    if (yuvFile) {
+        delete yuvFile;
+    }
 }
 
+void Player::on_OpenButton_clicked()
+{
+    openYuvFile();
+    imageLabel->setPixmap(QPixmap::fromImage(*yuv[0]));
+}
+
+void Player::on_updateButton_clicked()
+{
+    imageLabel->setPixmap(QPixmap::fromImage(*yuv[0]));
+}
+
+void Player::openYuvFile()
+{
+    yuvFile = new QFile("F:/seq/vvc/BasketballDrive_1920x1080_50.yuv");
+    yuvFile->open(QIODevice::ReadOnly);
+    yuvFile->read((char *)yuv[0]->bits(), width * height);
+}
--- player.h
+++ player.h
@@ -2,6 +2,10 @@
 #define PLAYER_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QScrollarea>
+
+#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Player; }
@@ -15,7 +19,21 @@ public:
     Player(QWidget *parent = nullptr);
     ~Player();
 
+private slots:
+    void on_updateButton_clicked();
+
+    void on_OpenButton_clicked();
+
 private:
     Ui::Player *ui;
+
+    QFile *yuvFile;
+    QImage *yuv[3];
+    int width;
+    int height;
+
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    void openYuvFile();
 };
 #endif // PLAYER_H
