a1debacdfd7ac4fea29d0161a57c43668517ea90
xuh <hanxu727@gmail.com>
2019-12-16 13:55:30 +0800

    convert yuv to rgb.

--- player.cpp
+++ player.cpp
@@ -1,8 +1,10 @@
 #include "player.h"
 #include "ui_player.h"
 
+#include <QtDebug>
+#include <QFileDialog>
+
 #include <QFile>
-#include <QDataStream>
 #include <QPixmap>
 
 Player::Player(QWidget *parent)
@@ -17,13 +19,13 @@ Player::Player(QWidget *parent)
     yuv[0] = new QImage(width, height, QImage::Format_Grayscale8);
     yuv[1] = new QImage(width >> 1, height >> 1, QImage::Format_Grayscale8);
     yuv[2] = new QImage(width >> 1, height >> 1, QImage::Format_Grayscale8);
+    rgb = new QImage(width, height, QImage::Format_RGB888);
 
     imageLabel = new QLabel();
     imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
 
     ui->scrollArea->setWidget(imageLabel);
     ui->scrollArea->setBackgroundRole(QPalette::Dark);
-
 }
 
 Player::~Player()
@@ -38,17 +40,66 @@ Player::~Player()
 void Player::on_OpenButton_clicked()
 {
     openYuvFile();
-    imageLabel->setPixmap(QPixmap::fromImage(*yuv[0]));
+    imageLabel->setPixmap(QPixmap::fromImage(*rgb));
 }
 
 void Player::on_updateButton_clicked()
 {
-    imageLabel->setPixmap(QPixmap::fromImage(*yuv[0]));
+    imageLabel->setPixmap(QPixmap::fromImage(*rgb));
 }
 
 void Player::openYuvFile()
 {
-    yuvFile = new QFile("F:/seq/vvc/BasketballDrive_1920x1080_50.yuv");
+    QString fileName = QFileDialog::getOpenFileName(this, tr("open YUV"), QString("F:/seq/vvc"));
+    ui->seqNameLabel->setText(fileName);
+
+
+    yuvFile = new QFile(fileName);
     yuvFile->open(QIODevice::ReadOnly);
-    yuvFile->read((char *)yuv[0]->bits(), width * height);
+    yuvFile->read(reinterpret_cast<char *>(yuv[0]->bits()), width * height);
+    yuvFile->read(reinterpret_cast<char *>(yuv[1]->bits()), (width * height) >> 2);
+    yuvFile->read(reinterpret_cast<char *>(yuv[2]->bits()), (width * height) >> 2);
+
+    uchar *yuvBuf[3] = {
+        yuv[0]->bits(),
+        yuv[1]->bits(),
+        yuv[2]->bits()
+    };
+
+    // TODO only bit depth smaller than 14 is supported
+    ColorSpaceCvt type = YUV2RGB_BT709_FULL;
+    const int* cvt = yuv2rgb[type];
+    int bitdepth = 8;
+    int lumaOffset = 16 << (bitdepth - 8);
+    int chromaOffset = 128 << (bitdepth - 8);
+    if (type == YUV2RGB_BT709_FULL || type == YUV2RGB_BT601_FULL || type == YUV2RGB_BT2020_FULL) {
+        lumaOffset = 0;
+    }
+    qDebug() << yuv[0]->bytesPerLine() << yuv[1]->bytesPerLine() << rgb[0].bytesPerLine();
+
+    int widthC = width / 2;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int pos = i * width + j;
+            int posC = (i / 2) * widthC + (j / 2);
+            int y = yuv[0]->bits()[pos] - lumaOffset;
+            int u = yuv[1]->bits()[posC] - chromaOffset;
+            int v = yuv[2]->bits()[posC] - chromaOffset;
+
+            //int y = 0 + yuv[0]->scanLine(i)[j] - lumaOffset;
+            //int u = 0 + yuv[1]->scanLine(i >> 1)[(j + 1) >> 1] - chromaOffset;
+            //int v = 0 + yuv[2]->scanLine(i >> 1)[(j + 1) >> 1] - chromaOffset;
+
+            int r = (cvt[0] * y +            + cvt[1] * v) >> (bitdepth + 8);
+            int g = (cvt[0] * y + cvt[2] * u + cvt[3] * v) >> (bitdepth + 8);
+            int b = (cvt[0] * y + cvt[4] * u             ) >> (bitdepth + 8);
+
+            // r = r < 0 ? 0: r > 255 ? 255: r;
+            // g = g < 0 ? 0: g > 255 ? 255: g;
+            // b = b < 0 ? 0: b > 255 ? 255: b;
+
+            rgb->setPixel(j, i, qRgb(r, g, b));
+        }
+    }
 }
--- player.h
+++ player.h
@@ -11,6 +11,25 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class Player; }
 QT_END_NAMESPACE
 
+enum ColorSpaceCvt {
+    YUV2RGB_BT709_LIMIT,
+    YUV2RGB_BT709_FULL,
+    YUV2RGB_BT601_LIMIT,
+    YUV2RGB_BT601_FULL,
+    YUV2RGB_BT2020_LIMIT,
+    YUV2RGB_BT2020_FULL,
+    YUV2RGB_TYPE_NUM
+};
+
+const int yuv2rgb[YUV2RGB_TYPE_NUM][12] = {
+    {76309, 117489, -13975, -34925, 138438}, // BT709_LimitedRange
+    {65536, 103206, -12276, -30679, 121608}, // BT709_FullRange
+    {76309, 104597, -25675, -53279, 132201}, // BT601_LimitedRange
+    {65536,  91881, -22553, -46802, 116129}, // BT601_FullRange
+    {76309, 110013, -12276, -42626, 140363}, // BT2020_LimitedRange
+    {65536,  96638, -10783, -37444, 123299}  // BT2020_FullRange
+};
+
 class Player : public QMainWindow
 {
     Q_OBJECT
@@ -29,6 +48,7 @@ private:
 
     QFile *yuvFile;
     QImage *yuv[3];
+    QImage *rgb;
     int width;
     int height;
 
